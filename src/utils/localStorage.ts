1f52894713067a88ee17394d4549253c27d75cb2
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2023-05-08 18:18:31 +0700

    feat: add function to retrive theme from local storage

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,30 @@
+interface ThemeData {
+    theme: "blue" | "green";
+}
+
+const getDataFromLocalStorage = (key: string): ThemeData | null => {
+    const data = localStorage.getItem(key);
+    if (data) {
+        try {
+            const parsedData = JSON.parse(data) as ThemeData;
+            return parsedData;
+        } catch (error) {
+            console.error('Error parsing data from local storage:', error);
+        }
+    }
+    return null;
+};
+
+const saveDataToLocalStorage = (data: ThemeData, key: string) => {
+    try {
+        const serializedData = JSON.stringify(data);
+        localStorage.setItem(key, serializedData);
+    } catch (error) {
+        console.error('Error saving data to local storage:', error);
+    }
+};
+
+export {
+    getDataFromLocalStorage,
+    saveDataToLocalStorage,
+}
