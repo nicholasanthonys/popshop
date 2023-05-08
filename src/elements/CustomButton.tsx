ebf0a02573bcdbe764d9f882441ee78d40f63865
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2023-05-08 12:29:08 +0700

    fix: fix component button

--- src/elements/CustomButton.tsx
+++ src/elements/CustomButton.tsx
@@ -1,28 +1,26 @@
 import React, { ChangeEvent, useState } from 'react';
 
 
-type ButtonProps = {
-    onClick: () => void;
-    disabled?: boolean;
-    className?: string;
-    children: React.ReactNode;
-  };
-  
-  const CustomButton: React.FC<ButtonProps> = ({
-    onClick,
-    disabled = false,
-    className = '',
-    children,
-  }) => {
-    return (
-      <button
-        onClick={onClick}
-        disabled={disabled}
-        className={`py-2 px-4 bg-[#3E76FF] hover:bg-[#3534FA] text-white drop-shadow-[0_4px_61px_rgba(62,118,255,0.4)] w-full font-medium rounded-md ${className}`}
-      >
-        {children}
-      </button>
-    );
-  };
-  
-  export default CustomButton;
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+};
+
+const CustomButton: React.FC<ButtonProps> = ({
+  onClick,
+  disabled = false,
+  className = '',
+  children,
+  type = "submit",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`py-2 px-4 bg-[#3E76FF] hover:bg-[#3534FA] text-white drop-shadow-[0_4px_61px_rgba(62,118,255,0.4)] w-full font-medium rounded-md ${className}  disabled:bg-slate-50 disabled:text-slate-500`}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default CustomButton;
