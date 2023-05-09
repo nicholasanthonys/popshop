ac3fe1517b2d672f433561aceb0df100378b7b73
Nicholas Anthony S <anthony.nicholas84@gmail.com>
2023-05-09 18:19:59 +0700

    feat: add missing image

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -17,7 +17,7 @@ function Dashboard() {
         <div className='mx-4'>
             <div className="grid grid-cols-6 gap-4">
                 <div className="col-span-6 sm:col-span-6">
-                    <div className="rounded-2xl overflow-hidden shadow-md flex p-4 py-4 items-center gap-6">
+                    <div className="bg-white rounded-2xl overflow-hidden shadow-md flex p-4 py-4 items-center gap-6">
                         <div className="flex-none">
                             <img className="w-full rounded-lg" src={profilePict} alt="profile picture" />
                         </div>
@@ -28,30 +28,37 @@ function Dashboard() {
                     </div>
                 </div>
                 <div className="col-span-6 sm:col-span-2">
-                    <div className="rounded-2xl overflow-hidden shadow-md flex p-4 py-4 items-center justify-between">
+                    <div className="bg-white rounded-2xl overflow-hidden shadow-md flex p-4 py-4 items-center justify-between">
                         <div >
-                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Contract Paid</p>
-                            <p className={`${currentTheme == 'green' ? 'text-[#6EB659]': 'text-[#3E74FB]'} text-2xl font-bold`}>20</p>
+                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Crypto Paid</p>
+                            <p className={`${currentTheme == 'green' ? 'text-[#6EB659]' : 'text-[#3E74FB]'} text-2xl font-bold`}>20</p>
                         </div>
-                        <div className={`h-14 w-14 bg-gradient-to-r  ${currentTheme == 'green' ? 'from-green-900 to-green-500': 'from-cyan-500 to-blue-500'} flex justify-center items-center rounded-lg`}>
+                        <div className={`h-14 w-14   ${currentTheme == 'green' ? 'bg-[#6EB659]' : 'bg-gradient-to-r from-cyan-500 to-blue-500'} flex justify-center items-center rounded-lg`}>
                             <img src={chart} alt='chart' className='m-auto' />
                         </div>
                     </div>
                 </div>
                 <div className="col-span-3 sm:col-span-2">
-                    <div className="rounded-2xl overflow-hidden shadow-md flex p-2 sm:p-4 py-4 ">
+                    <div className="bg-white rounded-2xl overflow-hidden shadow-md flex p-2 sm:p-4 py-4 items-center justify-between ">
                         <div>
-                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Contract In Progress</p>
+                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Crypto In Progress</p>
                             <p className='text-[#9F9F9F] text-2xl font-bold'>5</p>
 
+                        </div>
+
+                        <div className={`h-14 w-14   bg-gradient-to-br to-[#9B9B9B] from-[#B0B0B0] flex justify-center items-center rounded-lg`}>
+                            <img src={chart} alt='chart' className='m-auto' />
                         </div>
                     </div>
                 </div>
                 <div className="col-span-3 sm:col-span-2">
-                    <div className="rounded-2xl overflow-hidden shadow-md flex p-2 sm:p-4 py-4 items-center justify-between">
+                    <div className="bg-white rounded-2xl overflow-hidden shadow-md flex p-2 sm:p-4 py-4 items-center justify-between">
                         <div >
-                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Contract Unpaid</p>
+                            <p className='text-[#6D6E71] text-xs font-medium mb-2'>Crypto Unpaid</p>
                             <p className='text-[#D41919] text-2xl font-bold'>2</p>
+                        </div>
+                        <div className={`h-14 w-14   bg-gradient-to-br to-[#CF0808] from-[#FFCCCC] flex justify-center items-center rounded-lg`}>
+                            <img src={chart} alt='chart' className='m-auto' />
                         </div>
                     </div>
                 </div>
